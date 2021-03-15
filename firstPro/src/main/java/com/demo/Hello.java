5e69ab7ebf8dd016249df9c890a2dc4eac52f600
xcfirst <2889759825@qq.com>
2021-03-15 16:22:14 +0800

    创建分支

--- firstPro/src/main/java/com/demo/Hello.java
+++ firstPro/src/main/java/com/demo/Hello.java
@@ -15,5 +15,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("更新1");
         System.out.println("更新2");
+        System.out.println("创建分支");
     }
 }
